b5deb54e47ab6c150b99c014a5794120531d113b
Madhuri <gurubellig@gmail.com>
2021-09-06 18:52:48 +0530

    added some content

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -4,6 +4,19 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
 		System.out.println("This is second change");
+
+		String s = "Madhuri is good girl";
+		System.out.println(s.length());
+
+		// s = s.trim()
+		// s = s.replaceAll("\\s","");
+		System.out.println(s.length());
+
+		String[] newString = s.split(" ");
+		for (int i = 0; i < newString.length; i++) {
+			System.out.println(newString[i]);
+		}
+
 	}
 
 }
