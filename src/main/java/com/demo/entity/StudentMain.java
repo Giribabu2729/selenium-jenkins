8eb9deb9e033394757def5c2a7c7ec3ca8ea1d86
Madhuri <gurubellig@gmail.com>
2021-09-07 13:02:07 +0530

    added source to new branch selenium2.0

---
+++ src/main/java/com/demo/entity/StudentMain.java
@@ -0,0 +1,11 @@
+package com.demo.entity;
+
+public class StudentMain {
+	public static void main(String[] args) {
+
+		Student s1 = new Student(10, "giri", 43);
+		System.out.println(s1);
+
+	}
+
+}
