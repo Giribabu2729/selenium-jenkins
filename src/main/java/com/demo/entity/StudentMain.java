ae864b7cf9eeef5334daeafc7634031e72a0ca57
Madhuri <gurubellig@gmail.com>
2021-09-07 15:44:42 +0530

    added one more student object

--- src/main/java/com/demo/entity/StudentMain.java
+++ src/main/java/com/demo/entity/StudentMain.java
@@ -4,6 +4,7 @@ public class StudentMain {
 	public static void main(String[] args) {
 
 		Student s1 = new Student(10, "giri", 43);
+		Student s1 = new Student(20, "madhuri", 43);
 		System.out.println(s1);
 
 	}
